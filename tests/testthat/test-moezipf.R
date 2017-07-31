43c998a858c7faae852a41520ef64737b1db48a2
Ariel Duarte López <arieldl85@gmail.com>
2017-07-31 18:22:38 +0200

    reordering tests.

---
+++ tests/testthat/test-moezipf.R
@@ -0,0 +1,31 @@
+test_that("[moezipf - pmf] The summation of all probabilities must be 1.", {
+  expect_equal(1,sum(dmoezipf(1:1000, alpha = 2.5, beta = 0.10)), tolerance = 1*10^(-3))
+  expect_equal(1,sum(dmoezipf(1:1000, alpha = 2.5, beta = 1.3)), tolerance = 1*10^(-3))
+  expect_equal(1,sum(dmoezipf(1:1000, alpha = 2.5, beta = 3)), tolerance = 1*10^(-3))
+})
+
+test_that("[moezipf - pmf] The probability lays on the interval (0, 1).", {
+  pvalue <- dmoezipf(x = 9, alpha = 2, beta = 0.10)
+  expect_lt(pvalue, 1)
+  expect_gt(pvalue, 0)
+
+  pvalue <- dmoezipf(x = 9, alpha = 1.25, beta = 5.3)
+  expect_lt(pvalue, 1)
+  expect_gt(pvalue, 0)
+})
+
+test_that("[moezipf - cdf] The summation of all probabilities must be 1.", {
+  expect_equal(1,pmoezipf(1000, alpha = 2.5, beta = 0.10), tolerance = 1*10^(-3))
+  expect_equal(1,pmoezipf(1000, alpha = 2.5, beta = 1.3), tolerance = 1*10^(-3))
+  expect_equal(1,pmoezipf(1000, alpha = 2.5, beta = 3), tolerance = 1*10^(-3))
+})
+
+test_that("[moezipf- cdf] The cumulative probabilities have to be in the interval (0, 1).", {
+  pvalue <- pmoezipf(q = 9, alpha = 2, beta = 0.10)
+  expect_lt(pvalue, 1)
+  expect_gt(pvalue, 0)
+
+  pvalue <- pmoezipf(q = 9, alpha = 1.25, beta = 5.3)
+  expect_lt(pvalue, 1)
+  expect_gt(pvalue, 0)
+})
