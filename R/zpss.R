cc60515c87cc6e1ea01ec6b8cfe0076ad62cda6a
Ariel Duarte López <arieldl85@gmail.com>
2017-08-01 17:08:44 +0200

    Added functions d,p of the Z-PSS distributions. Added documentation and tests related with the Z-PSS.

---
+++ R/zpss.R
@@ -0,0 +1,109 @@
+#' The Zipf-Poisson Stop Sum Distribution (Z-PSS).
+#'
+#' Probability Mass Function, Cumulative Function of the Z-PSS distribution
+#' with parameter \eqn{\alpha} and \eqn{\lambda}.
+#'
+#' @name zpss
+#' @aliases dzpss
+#' @aliases pzpss
+#'
+#' @param x,q Vector of positive integer values.
+#' @param alpha Value of the \eqn{\alpha} parameter (\eqn{\alpha > 1} ).
+#' @param lambda Value of the \eqn{\lambda} parameter (\eqn{\lambda >= 0} ).
+#' @param log,log.p Logical; if TRUE, probabilities p are given as log(p).
+#' @param lower.tail Logical; if TRUE (default), probabilities are \eqn{P[X \leq x]}, otherwise, \eqn{P[X > x]}.
+#' @param isTruncated Logical; if TRUE, the truncated version of the distribution is returned.
+NULL
+#> NULL
+
+.prec.zpss.checkXvalue <- function(x){
+  if(!is.numeric(x) || x < 1 || x%%1 != 0) {
+    stop('The x value is not included into the support of the distribution.')
+  }
+}
+
+.prec.zpss.checkparams <- function(alpha, lambda){
+  if(!is.numeric(alpha) || alpha <= 1){
+    stop('Incorrect alpha parameter. This parameter should be greater than one.')
+  }
+
+  if(!is.numeric(lambda) || lambda < 0){
+    stop('Incorrect lambda parameter. You should provide a numeric value.')
+  }
+}
+
+.panjerRecursion <- function(k, alpha, lambda){
+  p0 <- exp(-lambda)
+
+  if(k == 0){
+    return(p0)
+  }
+
+  probs <- array(0, k + 1)
+  probs[1] <- p0
+  z_a <- VGAM::zeta(alpha)
+
+  for(i in 1:k){
+    probs[i+1] <- (lambda/(i*z_a)) * sum((1:i)^(-alpha + 1) * probs[i:1])
+  }
+  return(probs)
+}
+
+#' @rdname zpss
+#' @export
+dzpss <- function(x, alpha, lambda, log = FALSE, isTruncated = FALSE){
+  k <- max(x)
+  .prec.zpss.checkXvalue(k)
+  .prec.zpss.checkparams(alpha, lambda)
+
+  probs <- .panjerRecursion(k, alpha, lambda)
+
+  if(isTruncated){
+    probs <- (probs)/(1 - probs[1])
+    probs <- probs[-1]
+  }
+
+  finalProbs <- probs[x]
+  if(log){
+    return(log(finalProbs))
+  }
+
+  return(finalProbs)
+}
+
+#' @rdname zpss
+#' @export
+pzpss <- function(q, alpha, lambda, log.p = FALSE, lower.tail = TRUE, isTruncated = FALSE){
+  k <- max(q)
+  .prec.zpss.checkXvalue(k)
+  .prec.zpss.checkparams(alpha, lambda)
+
+  probs <- .panjerRecursion(k, alpha, lambda)
+  if(isTruncated){
+    probs <- (probs)/(1 - probs[1])
+    probs <- probs[-1]
+  }
+
+  finalProbs <- array(0, length(q))
+  finalProbs <- sapply(1:length(q), function(i, q, probs){
+    finalProbs[i] <- sum(probs[1:q[i]])
+  }, q = q, probs, probs)
+  #finalProbs <- probs[q]
+  if(!log.p & lower.tail){
+    return(finalProbs)
+  } else{
+    if(!log.p & !lower.tail){
+      return(1 - finalProbs)
+    } else{
+      if(log.p & !lower.tail){
+        return(log(1 - finalProbs))
+      }
+      return(log(finalProbs))
+    }
+  }
+}
+
+
+
+
+
--- tests/testthat/test-zpe.R
+++ tests/testthat/test-zpe.R
@@ -6,6 +6,7 @@ test_that("[zpe] Checking preconditions", {
   expect_error(pzpe(0, 2.5, -1.5), "The x value is not included into the support of the distribution.")
   expect_error(dzpe(1.6, 2.5, -1.5), "The x value is not included into the support of the distribution.")
 })
+
 test_that("[zpe - pmf] The summation of all probabilities must be 1.", {
   expect_equal(1,sum(dzpe(1:1000, alpha = 2.5, beta = 1.3)), tolerance = 1*10^(-3))
   expect_equal(1,sum(dzpe(1:1000, alpha = 2.5, beta = -3)), tolerance = 1*10^(-3))
