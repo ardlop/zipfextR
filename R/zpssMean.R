7ff2e1794ddfc3ee54f0643377d58195055430da
Ariel Duarte López <arieldl85@gmail.com>
2017-10-11 18:24:20 +0200

    updated package

--- R/zpssMean.R
+++ R/zpssMean.R
@@ -1,6 +1,6 @@
 #' Expected value of the Z-PSS distribution.
 #'
-#' Computes the expected value of the Z-PSS distribution for given values of parameters.
+#' Computes the expected value of the Z-PSS distribution for given values of parameters
 #' \eqn{\alpha} and \eqn{\lambda}.
 #'
 #' @param alpha Value of the \eqn{\alpha} parameter (\eqn{\alpha > 2}).
@@ -11,7 +11,15 @@
 #'
 #' @details
 #' The expected value of the Z-PSS distribution only exists for \eqn{\alpha} values strictly greater than 2.
+#' The value is derive from \eqn{E[Y] = E[N]\, E[X]} where E[X] is the mean value of the Zipf distribution and
+#' E[N] is the expected value of a Poisson one. The resulting expression is set to be equal to:
+#'\deqn{E[Y] = \lambda\, \frac{\zeta(\alpha - 1)}{\zeta(\alpha)}}.
+#'Particularlly, if one is dealing with the zero-truncated version of the Z-PSS distribution. This values es calculated as:
+#'\deqn{E[Y^{ZT}] = \frac{\lambda\, \zeta(\alpha - 1)}{\zeta(\alpha)\, (1 - e^{-\lambda})}}
 #'
+#' @references {
+#' Sarabia Alegría, JM. and Gómez Déniz, E. and Vázquez Polo, F. Estadística actuarial: teoría y aplicaciones. Pearson Prentice Hall.
+#' }
 #' @examples
 #' zpssMean(2.5, 1.3)
 #' zpssMean(2.5, 1.3, TRUE)
@@ -26,7 +34,7 @@ zpssMean <- function(alpha, lambda, isTruncated = FALSE){
   }
   zeta_a <- VGAM::zeta(alpha)
   if(!isTruncated){
-    lambda * (VGAM::zeta(alpha - 1)/zeta_a)
+    return(lambda * (VGAM::zeta(alpha - 1)/zeta_a))
   }
   lambda*VGAM::zeta(alpha - 1)/zeta_a*(1 - exp(-lambda))
 }
--- R/zpssVariance.R
+++ R/zpssVariance.R
@@ -1,3 +1,41 @@
-zpssVariance <- function(){
+#' Variance of the Z-PSS distribution.
+#'
+#' Computes the variance of the Z-PSS distribution for given values of parameters
+#' \eqn{\alpha} and \eqn{\lambda}.
+#'
+#' @param alpha Value of the \eqn{\alpha} parameter (\eqn{\alpha > 3}).
+#' @param lambda Value of the \eqn{\lambda} parameter (\eqn{\lambda \geq 0}).
+#' @param isTruncated Logical; if TRUE Use the zero-truncated version of the distribution to calculate the expected value (default = FALSE).
+#'
+#' @return A positive real value corresponding to the variance of the distribution.
+#'
+#' @details
+#' The variance of the Z-PSS distribution only exists for \eqn{\alpha} values strictly greater than 3.
+#' The value is derive from \eqn{Var[Y] = E[N]\, Var[X] + E[X]^2 \, Var[N]} where E[X] and E[N] ares the expected value of the
+#' Zipf and the Poisson distributions respectively. In the same way the values of Var[X] and Var[N] stand for the variances of the Zipf and the Poisson
+#' distributions. The resulting expression is set to be equal to:
+#' \deqn{Var[Y] = \lambda\, \frac{\zeta(\alpha - 2)}{\zeta(\alpha)}}.
+#' Particularlly, the variance of the zero-truncated version of the Z-PSS distribution is calculated as:
+#' \deqn{Var[Y^{ZT}] = \frac{\lambda\, \zeta(\alpha)\, \zeta(\alpha - 2)\, (1 - e^{-\lambda}) - \lambda^2 \, \zeta(\alpha - 1)^2 \, e^{-\lambda}}{\zeta(\alpha)^2 \, (1 - e^{-\lambda})^2}}
+#'
+#' @references {
+#' Sarabia Alegría, JM. and Gómez Déniz, E. and Vázquez Polo, F. Estadística actuarial: teoría y aplicaciones. Pearson Prentice Hall.
+#' }
+#' @examples
+#' zpssVariance(4.5, 2.3)
+#' zpssVariance(4.5, 2.3, TRUE)
+#' @export
+zpssVariance <- function(alpha, lambda, isTruncated = FALSE){
+  if(!is.numeric(alpha) || !is.numeric(lambda)){
+    stop("Wrong input parameters!!")
+  }
 
+  if(alpha < 3){
+    stop('The alpha parameter must be greater than 2.')
+  }
+  zeta_a <- VGAM::zeta(alpha)
+  if(!isTruncated){
+    return(lambda*VGAM::zeta(alpha - 2)/zeta_a)
+  }
+  (lambda*zeta_a*VGAM::zeta(alpha - 2)*(1-exp(-lambda)) - lambda^2 * VGAM::zeta(alpha - 1)^2 * exp(-lambda))/(zeta_a^2 * (1 - exp(-lambda))^2)
 }
